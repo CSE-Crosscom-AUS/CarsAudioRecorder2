385054bfbfa9042474747befaa43966c72a9d175
Doug Partridge <doug.partridge@cse-crosscom.com.au>
2021-08-23 16:06:45 +1000

    write behringer to 4 separate wav files at 48kHz (ready for opus)

--- CarsAudioRecorder2b/Program.cs
+++ CarsAudioRecorder2b/Program.cs
@@ -70,40 +70,118 @@ namespace CarsAudioRecorder2b
 
 
 
+                //NAudio.Wave.WaveFormat opus_format4 = new NAudio.Wave.WaveFormat(48000, 16, 4);
 
+                int chanel_count;
 
+                chanel_count = 4;
+                NAudio.Wave.WaveFormat opus_format4 = NAudio.Wave.WaveFormat.CreateCustomFormat(NAudio.Wave.WaveFormatEncoding.Pcm, 48000, chanel_count, 48000 * chanel_count * 2, chanel_count * 2, 16);
 
-                NAudio.Wave.MultiplexingWaveProvider[] mwp = new NAudio.Wave.MultiplexingWaveProvider[4];
+                chanel_count = 1;
+                NAudio.Wave.WaveFormat opus_format1 = NAudio.Wave.WaveFormat.CreateCustomFormat(NAudio.Wave.WaveFormatEncoding.Pcm, 48000, chanel_count, 48000 * chanel_count * 2, chanel_count * 2, 16);
 
-                for (int i = 0; i < mwp.Length; i++)
+                NAudio.Wave.WaveFileWriter[] wave = new NAudio.Wave.WaveFileWriter[4];
+
+                for (int i = 0; i < wave.Length; i++)
                 {
-                    mwp[i] = new NAudio.Wave.MultiplexingWaveProvider(new NAudio.Wave.IWaveProvider[] { bwp[i], }, 1);
-                    mwp[i].ConnectInputToOutput(i, 0);
+                    wave[i] = new NAudio.Wave.WaveFileWriter($"out{i}.wav", opus_format1);
                 }
 
 
+                byte[] buffer = new byte[capture.WaveFormat.BitsPerSample * capture.WaveFormat.Channels / 8 + 1000];
 
 
+                object wave_lock = new object();
 
                 capture.DataAvailable += (object sender, NAudio.Wave.WaveInEventArgs e) =>
                 {
-                    for (int i = 0; i < bwp.Length; i++)
+
+                    lock (wave_lock)
                     {
-                        bwp[i].AddSamples(e.Buffer, 0, e.BytesRecorded);
+                        for (int i = 0; i < bwp.Length; i++)
+                        {
+                            bwp[i].AddSamples(e.Buffer, 0, e.BytesRecorded);
+
+                            if (bwp[i].BufferLength >= 48000 * capture.WaveFormat.BitsPerSample * capture.WaveFormat.Channels / 8)
+                            {
+                                Console.WriteLine($"buffer duration is {bwp[i].BufferDuration.TotalMilliseconds}");
+
+                                Console.WriteLine("hi");
+
+
+                                NAudio.Wave.ResamplerDmoStream rds = new NAudio.Wave.ResamplerDmoStream(bwp[i], opus_format4);
+
+                                NAudio.Wave.MultiplexingWaveProvider mwp2 = new NAudio.Wave.MultiplexingWaveProvider(new NAudio.Wave.IWaveProvider[] { rds, }, 1);
+                                mwp2.ConnectInputToOutput(i, 0);
+
+
+                                //while (true)
+                                //{
+                                //    int count = rds.Read(buffer, 0, buffer.Length);
+                                //    if (count == 0)
+                                //    {
+                                //        break;
+                                //    }
+
+                                //    wave.Write(buffer, 0, count);
+                                //}
+
+                                while (true)
+                                {
+
+                                    int count = mwp2.Read(buffer, 0, buffer.Length);
+
+                                    if (count == 0)
+                                    {
+                                        break;
+                                    }
+
+                                    wave[i].Write(buffer, 0, count);
+                                }
+
+                            }
+
+
+
+                            bwp[i].ClearBuffer();
+                        }
                     }
                 };
 
 
 
-                NAudio.Wave.IWavePlayer[] wo = new DiskWavePlayer[4];
 
-                for (int i = 0; i < wo.Length; i++)
-                {
-                    wo[i] = new DiskWavePlayer($"out{i}.wav");
+                //NAudio.Wave.MultiplexingWaveProvider[] mwp = new NAudio.Wave.MultiplexingWaveProvider[4];
 
-                    wo[i].Init(mwp[i]);
-                    wo[i].Play();
-                }
+                //for (int i = 0; i < mwp.Length; i++)
+                //{
+                //    mwp[i] = new NAudio.Wave.MultiplexingWaveProvider(new NAudio.Wave.IWaveProvider[] { bwp[i], }, 1);
+                //    mwp[i].ConnectInputToOutput(i, 0);
+                //}
+
+
+
+
+
+                //capture.DataAvailable += (object sender, NAudio.Wave.WaveInEventArgs e) =>
+                //{
+                //    for (int i = 0; i < bwp.Length; i++)
+                //    {
+                //        bwp[i].AddSamples(e.Buffer, 0, e.BytesRecorded);
+                //    }
+                //};
+
+
+
+                //NAudio.Wave.IWavePlayer[] wo = new DiskWavePlayer[4];
+
+                //for (int i = 0; i < wo.Length; i++)
+                //{
+                //    wo[i] = new DiskWavePlayer($"out{i}.wav");
+
+                //    wo[i].Init(mwp[i]);
+                //    wo[i].Play();
+                //}
 
 
 
@@ -123,12 +201,19 @@ namespace CarsAudioRecorder2b
                 capture.StopRecording();
 
 
-                for (int i = 0; i < wo.Length; i++)
-                {
-                    wo[i].Stop();
-                    wo[i].Dispose();
-                }
+                //for (int i = 0; i < wo.Length; i++)
+                //{
+                //    wo[i].Stop();
+                //    wo[i].Dispose();
+                //}
 
+                lock (wave_lock)
+                {
+                    for (int i = 0; i < wave.Length; i++)
+                    {
+                        wave[i].Dispose();
+                    }
+                }
             }
         }
     }
